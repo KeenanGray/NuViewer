72cd36dc9d7c11c24753001ad07f916c56a010b7
Keenan Gray <keenangray@Keenans-MacBook-Pro-2.local>
2020-07-02 09:53:50 -0400

    add ball-game

--- Assets/Scripts/Components/ReActions/Rotate.cs
+++ Assets/Scripts/Components/ReActions/Rotate.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+    public Vector3 directionAndSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(directionAndSpeed);
     }
 }
